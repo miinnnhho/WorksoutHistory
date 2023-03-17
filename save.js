e5b25b791d5df243db702dc2b181605afca9a734
minho <102772287+miinnnhho@users.noreply.github.com>
2023-03-17 19:36:30 +0900

    ver 1.0.0

---
+++ save.js
@@ -0,0 +1,10 @@
+function saveValue() {
+  let exercise = document.getElementById('exercise').value;
+  let sets = document.getElementById('sets').value;
+  let reps = document.getElementById('reps').value;
+  let weight = document.getElementById('weight').value;
+
+  let saveResult = (document.getElementById('result').innerHTML = `${exercise}: ${sets}세트 ${reps}회 (${weight} kg)`);
+}
+
+
