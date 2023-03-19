2970306fdef1c1154ab1df1124702bc13a8075ed
minho <102772287+miinnnhho@users.noreply.github.com>
2023-03-19 14:43:47 +0900

    update
    add today date, add <br> tag, add output Box

--- save.js
+++ save.js
@@ -1,10 +1,15 @@
+// 결과 출력
 function saveValue() {
   let exercise = document.getElementById('exercise').value;
   let sets = document.getElementById('sets').value;
   let reps = document.getElementById('reps').value;
   let weight = document.getElementById('weight').value;
 
-  let saveResult = (document.getElementById('result').innerHTML = `${exercise}: ${sets}세트 ${reps}회 (${weight} kg)`);
+  let saveResult = (document.getElementById(
+    'result'
+  ).innerHTML += `${exercise}: ${sets}세트 ${reps}회 (${weight} kg)`+'<br>');
+
+  
 }
 
 
